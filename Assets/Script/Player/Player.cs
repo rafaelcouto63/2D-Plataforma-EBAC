04329db53b241bc566dbadd3742de85305553d06
rafaelcouto63 <rafacouto63@gmail.com>
2025-01-04 16:44:27 -0300

    Player Jump

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public Vector2 velocity;
-
+    public Vector2 friction = new Vector2(-.1f,0);
     public float speed;
+
+    public float forcejump = 20;
    
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        Movement();
+        Jump();
+    }
+
+    private void Movement()
+    {
+      if(Input.GetKey(KeyCode.LeftArrow))
         {
            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
@@ -21,5 +28,23 @@ public class Player : MonoBehaviour
            // myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        
+        if(myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+    }
+
+    private void Jump()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+        myRigidbody.velocity = Vector2.up * forcejump;
+       }
     }
 }
