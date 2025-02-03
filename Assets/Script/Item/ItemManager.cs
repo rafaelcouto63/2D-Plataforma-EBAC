1bb1026159668dab696ed670de771f3f63318d1c
rafaelcouto63 <rafacouto63@gmail.com>
2025-02-03 13:09:09 -0300

    Add Coin Counter HUD

--- Assets/Script/Item/ItemManager.cs
+++ Assets/Script/Item/ItemManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemManager : MonoBehaviour
 {
     public static ItemManager instance;
     public int coins;
+    public TextMeshProUGUI coinText;
 
     private void Awake() 
     {
@@ -27,10 +29,17 @@ public class ItemManager : MonoBehaviour
     private void Reset()
     {
         coins = 0;
+        UpdateCoinText();
     }
 
     public void AddCoin(int amount = 1)
     {
         coins += amount;
+        UpdateCoinText();
+    }
+
+    private void UpdateCoinText()
+    {
+        coinText.text = "X " + coins.ToString(); 
     }
 }
