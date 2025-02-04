5384998fd7442fbcf2d09ffbd600e1baa221c881
rafaelcouto63 <rafacouto63@gmail.com>
2025-02-04 15:56:45 -0300

    Add particles to Coins

--- Assets/Script/Item/CollectableBase.cs
+++ Assets/Script/Item/CollectableBase.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class CollectableBase : MonoBehaviour
 {
     public string compareTag = "Player";
+    public ParticleSystem particleSystem;
+
+    private void Awake()
+    {
+        if(particleSystem != null) 
+        {
+            particleSystem.transform.SetParent(null);
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.transform.CompareTag(compareTag)) 
@@ -20,7 +29,10 @@ public class CollectableBase : MonoBehaviour
     
     protected virtual void OnCollect()
     {
-
+       if(particleSystem != null) 
+       {
+          particleSystem.Play();
+       }
     }
 
    
